b9e723a1fad58ee5f39b9e7ab96de4969ad6061a
Guilherme-07062002 <guilherme762002@gmail.com>
2023-08-08 13:49:42 -0300

    ex02

---
+++ ex02.php
@@ -0,0 +1,5 @@
+<?php
+// Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas para ela:
+$nome = readline("Informe o seu nome: ");
+echo "Olá $nome, seja bem vindo.\n";
+?>
